8ae869b450619bdb21ec73f8af0df8b18148b2ae
Paulalbo <65129276+Paulalbo@users.noreply.github.com>
2020-07-13 15:05:46 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -9,15 +9,28 @@
 <body class="exercise2">
 <h4>CONVERT <br><strong>°F</strong> IN <strong>°C</strong></h4>
     <div>
-        <input type="number" name="convert" id="convert"></input>
-        <button type="button" id="calculate">calculate</button>
+        <form method="post">
+            <input type="number" name="convert" id="convert"></input>
+            <button type="button" id="calculate" href="">calculate</button>
+        </form>
 
     </div>
 
     <?php
-        //$value =  $GET['convert'];
-        //echo $value
-        //echo $_POST['convert'];
+
+        $e=15;
+        
+        if ($e<=5 )
+        echo "<h4>very cold</h4>";
+        elseif ($e<=10 )
+        echo "<h4>Cold</h4>";
+        elseif ($e<=15 )
+        echo "<h4>Pleasant</h4>";
+        elseif ($e<=20 )
+        echo "<h4>Warm</h4>";
+        elseif ($e>21 )
+        echo "<h4>hot</h4>";
+        
     ?>
 </body>
 </html>
