e062d9a14d5e8273285f4a8c1d2c7e68fce761cd
Paulalbo <65129276+Paulalbo@users.noreply.github.com>
2020-07-13 14:10:48 +0200

    first exercise done --> second in progress

---
+++ index2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./main.css">
+    <title>2nd exercise</title>
+</head>
+<body class="exercise2">
+<h4>CONVERT <br><strong>°F</strong> IN <strong>°C</strong></h4>
+    <div>
+        <input type="number" name="convert" id="convert"></input>
+        <button type="button" id="calculate">calculate</button>
+
+    </div>
+
+    <?php
+        //$value =  $GET['convert'];
+        //echo $value
+        //echo $_POST['convert'];
+    ?>
+</body>
+</html>
